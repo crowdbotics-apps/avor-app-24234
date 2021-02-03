6efb69f5acd9466b42b5630aa2c31ad5cd646609
Crowdbotics <team@crowdbotics.com>
2021-02-03 10:31:01 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'BlankScreen14199991', human_name: 'Blank Screen 14', access_route: 'BlankScreen14199991'},
 { name: 'Settings199990', human_name: 'Settings', access_route: 'Settings199990'},
 { name: 'UserProfile199983', human_name: 'User Profile', access_route: 'UserProfile199983'},
-{ name: 'Settings199982', human_name: 'Settings', access_route: 'Settings199982'},
 { name: 'Settings199980', human_name: 'Settings', access_route: 'Settings199980'},
 { name: 'SignIn2199978', human_name: 'Sign In 2', access_route: 'SignIn2199978'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import BlankScreen14199991Navigator from '../features/BlankScreen14199991/navigator';
 import Settings199990Navigator from '../features/Settings199990/navigator';
 import UserProfile199983Navigator from '../features/UserProfile199983/navigator';
-import Settings199982Navigator from '../features/Settings199982/navigator';
 import Settings199980Navigator from '../features/Settings199980/navigator';
 import SignIn2199978Navigator from '../features/SignIn2199978/navigator';
 
@@ -22,7 +21,6 @@ const AppNavigator = {
 BlankScreen14199991: { screen: BlankScreen14199991Navigator },
 Settings199990: { screen: Settings199990Navigator },
 UserProfile199983: { screen: UserProfile199983Navigator },
-Settings199982: { screen: Settings199982Navigator },
 Settings199980: { screen: Settings199980Navigator },
 SignIn2199978: { screen: SignIn2199978Navigator },
 
